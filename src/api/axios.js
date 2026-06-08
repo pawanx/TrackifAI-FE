a5be18917f5f2b5a74c19e652eb8e25cc2ff2a41
Pawan <pawanmishra196@gmail.com>
2026-06-08 09:49:24 +0530

    chore: local to production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const VITE_API_URL="https://trackifai-backend1.onrender.com/api"
-// baseURL: import.meta.env.VITE_API_URL
+//const VITE_API_URL="https://trackifai-backend1.onrender.com/api"
+// ;baseURL : "http://localhost:5000/api"
 const API = axios.create({
-    baseURL : "http://localhost:5000/api"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 API.interceptors.request.use((config) => {
